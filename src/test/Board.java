c3a3f34b774f8749333fe6ade6bd2f5e1c757628
bendyoni <bendyoni@gmail.com>
2024-03-31 19:53:06 +0300

    המשך דיבאג של מתודת getWord ומתודת getScore וסיום

--- src/test/Board.java
+++ src/test/Board.java
@@ -1,8 +1,6 @@
 package test;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 
 public class Board {
     private static Board b = null; // for singleton
@@ -58,10 +56,6 @@ public class Board {
                 return false;
         }
 
-       // if (null == GameBoard[7][7]) {
-       //     return word.getRow() == 7 && word.getCol() == 7;
-       // }
-
         for (int i=0; i<tiles.length; i++) {
             r = vertical ? row + i : row;
             c = vertical ? col : col + i;
@@ -69,39 +63,10 @@ public class Board {
             // Checks if the user is trying to use a existing letter on the board
             if (word.tiles[i] == null && GameBoard[r][c] == null)
                 return false;   
-            
-///////////////////////////////////////////////////////////////////////////
-           /*
-            if (GameBoard[currentRow][currentCol] == null) {
-                boolean anchored = false;
-                if (vertical) {
-                    anchored = (currentCol > 0 && GameBoard[currentRow][currentCol - 1] != null) ||
-                               (currentCol < 14 && GameBoard[currentRow][currentCol + 1] != null);
-                } else {
-                    anchored = (currentRow > 0 && GameBoard[currentRow - 1][currentCol] != null) ||
-                               (currentRow < 14 && GameBoard[currentRow + 1][currentCol] != null);
-                }
-                if (!anchored) {
-                    return false;
-                }
-                
-                
-            }
-            
-            if (vertical) {
-                row++;
-            } else {
-                col++;
-            } 
-            
-            ////////////////////////////////////////////////////////////////////
-            */
         } 
         return true;
     }
 
-    
-   
        public boolean dictionaryLegal(Word word){
         /*
          Set<String> seenWords = new HashSet<>();
@@ -135,215 +100,73 @@ public class Board {
         
             words.add(word);
         
-            // // Check for new words formed by the placed tiles
-            // for (int i = 0; i < tiles.length; i++) {
-            //     int r = vertical ? row + i : row;
-            //     int c = vertical ? col : col + i;
-        
-            //     if (vertical) {
-            //         addWordIfValid(words, tiles[i], r, c, false);
-            //     } else {
-            //         addWordIfValid(words, tiles[i], r, c, true);
-            //     }
-            // }
-        
             // Check for existing words that intersect with the placed tiles
             for (int i = 0; i < tiles.length; i++) {
                 int r = vertical ? row + i : row;
                 int c = vertical ? col : col + i;
         
-                // Check for horizontal words
-                if (c > 0 && GameBoard[r][c - 1] != null) {
-                    addWordIfValid(words, GameBoard[r][c - 1], r, c - 1, false);
-                }
-                if (c < 14 && GameBoard[r][c + 1] != null) {
-                    addWordIfValid(words, GameBoard[r][c + 1], r, c + 1, false);
-                }
-        
-                // Check for vertical words
-                if (r > 0 && GameBoard[r - 1][c] != null) {
-                    addWordIfValid(words, GameBoard[r - 1][c], r - 1, c, true);
-                }
-                if (r < 14 && GameBoard[r + 1][c] != null) {
-                    addWordIfValid(words, GameBoard[r + 1][c], r + 1, c, true);
-                }
+            if (tiles[i] == null) continue; // we're using a preexisting letter, not creating new words
+
+                addWordIfValid(words, tiles[i], r, c, vertical);
             }
         
             return words;
-        }
+        }   
     
         private void addWordIfValid(ArrayList<Word> words, Tile tile, int row, int col, boolean vertical) {
-            ArrayList<Tile> wordTilesDown = new ArrayList<>();
-            ArrayList<Tile> wordTilesRight = new ArrayList<>();
-            ArrayList<Tile> wordTilesTop = new ArrayList<>();
-            ArrayList<Tile> wordTilesLeft = new ArrayList<>();
-           
-            wordTilesDown.add(tile);
-            wordTilesRight.add(tile);
-            wordTilesTop.add(tile);
-            wordTilesLeft.add(tile);
+            ArrayList<Tile> wordTilesTopBottom = new ArrayList<>();
+            ArrayList<Tile> wordTilesLeftToRight = new ArrayList<>();
 
             int r = row;
             int c = col;
-            StringBuilder sbForword = new StringBuilder();
-            StringBuilder sbBack = new StringBuilder();
-            sbForword.append(tile.letter);
-            sbBack.append(tile.letter);
 
-    
-            while (r >= 0 && r < 15 && c >= 0 && c < 15 && GameBoard[r][c] != null) {
-                r++;
-                wordTilesDown.add(GameBoard[r][c]);                   
-            }
+            if (!vertical) { // if we're vertical we don't want to add vertical words - they're our new word!
+                // Only add horizontals
+                wordTilesTopBottom.add(tile);
+                while (r >= 0 && r < 15 && c >= 0 && c < 15 && GameBoard[r + 1][c] != null) {
+                    r++;
+                    wordTilesTopBottom.add(GameBoard[r][c]);
+                }
+                r = row;
+                while (r >= 0 && r < 15 && c >= 0 && c < 15 && GameBoard[r - 1][c] != null) {
+                    r--;
+                    wordTilesTopBottom.add(0, GameBoard[r][c]);
+                }
 
-            if (wordTilesDown.size() > 1) {
-                    Tile[] wordTilesArray = wordTilesDown.toArray(new Tile[0]);
-                    Word newWord = new Word(wordTilesArray, vertical ? row : r, vertical ? c : col, vertical);
+                if (wordTilesTopBottom.size() > 1) {
+                    Tile[] wordTilesArray = wordTilesTopBottom.toArray(new Tile[0]);
+                    Word newWord = new Word(wordTilesArray, r, c, true);
                     if (dictionaryLegal(newWord)) {
                         words.add(newWord);
                     }
                 }
-            r = row;
-            c = col;
-
-            while (r >= 0 && r < 15 && c >= 0 && c < 15 && GameBoard[r][c] != null) {
+            } else { // if our new word is horizontal, only add verticals
+                wordTilesLeftToRight.add(tile);
+                while (r >= 0 && r < 15 && c >= 0 && c < 15 && GameBoard[r][c + 1] != null) {
                     c++;
-                    wordTilesRight.add(GameBoard[r][c]);
-                }                   
+                    wordTilesLeftToRight.add(GameBoard[r][c]);
+                }
+                c = col;
+                while (r >= 0 && r < 15 && c >= 0 && c < 15 && GameBoard[r][c - 1] != null) {
+                    c--;
+                    wordTilesLeftToRight.add(0, GameBoard[r][c]);
+                }
 
-            if (wordTilesRight.size() > 1) {
-                    Tile[] wordTilesArray = wordTilesRight.toArray(new Tile[0]);
-                    Word newWord = new Word(wordTilesArray, vertical ? row : r, vertical ? c : col, vertical);
+
+                if (wordTilesLeftToRight.size() > 1) {
+                    Tile[] wordTilesArray = wordTilesLeftToRight.toArray(new Tile[0]);
+                    Word newWord = new Word(wordTilesArray, r, c, false);
                     if (dictionaryLegal(newWord)) {
                         words.add(newWord);
                     }
                 }
-    
-            r = row;
-            c = col;
-    
-            while (r >= 0 && r < 15 && c >= 0 && c < 15 && GameBoard[r][c] != null) {
-                r--;
-                wordTilesTop.add(GameBoard[r][c]);
             }
-
-                if (wordTilesTop.size() > 1) {
-                    Collections.reverse(wordTilesTop);
-                    Tile[] wordTilesArray = wordTilesTop.toArray(new Tile[0]);
-                    Word newWord = new Word(wordTilesArray, vertical ? row : r, vertical ? c : col, vertical);
-                    if (dictionaryLegal(newWord)) {
-                        words.add(newWord);
-                    }
-            }
-
-            r = row;
-            c = col;
-
-            while (r >= 0 && r < 15 && c >= 0 && c < 15 && GameBoard[r][c] != null) {
-                c--;
-                wordTilesLeft.add(GameBoard[r][c]);
-            }
-
-                if (wordTilesLeft.size() > 1) {
-                    Collections.reverse(wordTilesLeft);
-                    Tile[] wordTilesArray = wordTilesLeft.toArray(new Tile[0]);
-                    Word newWord = new Word(wordTilesArray, vertical ? row : r, vertical ? c : col, vertical);
-                    if (dictionaryLegal(newWord)) {
-                        words.add(newWord);
-                    }
-            }
-
         }
-    
-
-    //    public ArrayList<Word> getWords(Word word){
-    //    ArrayList<Word> wordsArr = new ArrayList<>();
-    //    Tile[] tiles = word.getTiles();
-    //    int row = word.getRow();
-    //    int col = word.getCol();
-    //    boolean vertical = word.isVertical();
-    //    int r;
-    //    int c;
-
-    //    wordsArr.add(word); //Add the current word to the list
-
-    //    for (int i=0; i<tiles.length; i++) {
-    //         r = vertical ? row + i : row;
-    //         c = vertical ? col : col + i;
-
-    //         if(vertical) {
-    //             checkVerticalWords(wordsArr, r, c, GameBoard[r][c]);
-    //             col++;
-    //         }
-    //          else {
-    //              checkBalancedWords(wordsArr, r, c, GameBoard[r][c]);
-    //              row++;
-    //         }
-    //      }
-        
-    //     return wordsArr;
-    // }
-
-    // private void checkVerticalWords(ArrayList<Word> wordsArr, int row, int col, Tile tile) {
-    //     // search for new vertical words form the given tile
-    //     StringBuilder sb = new StringBuilder();
-    //     int startRow = row;
-    //     while (0 <= startRow && GameBoard[startRow][col] != null) {
-    //         startRow--;
-    //     }
-    //     startRow++;
-
-    //     int endRow = row;
-    //     while (endRow < 15 && GameBoard[endRow][col] != null) {
-    //         endRow++;
-    //     }
-
-    //     for (int r = startRow; r < endRow; r++) {
-    //         sb.append(GameBoard[r][col].letter);
-    //     }
-
-    //     if (1 < sb.length()) {
-    //         wordsArr.add(new Word(getTilesFromString(sb.toString()), startRow, col, true));
-    //     }
-    // }
-
-    // private void checkBalancedWords(ArrayList<Word> wordsArr, int row, int col, Tile tile) {
-    //     // search for new horizontal words formed by the given tile
-    //     StringBuilder sb = new StringBuilder();
-    //     int startCol = col;
-    //     while (0 <= startCol && GameBoard[row][startCol] != null) {
-    //         startCol--;
-    //     }
-    //     startCol++;
-
-    //     int endCol = col;
-    //     while (endCol < 15 && GameBoard[row][endCol] != null) {
-    //         endCol++;
-    //     }
-
-    //     for (int c = startCol; c < endCol; c++) {
-    //         sb.append(GameBoard[row][c].letter);
-    //     }
-
-    //     if (sb.length() > 1) {
-    //         wordsArr.add(new Word(getTilesFromString(sb.toString()), row, startCol, false));
-    //     }
-    // }
-
-    // private Tile[] getTilesFromString(String str) {
-    //     // Convert a string to an array of Tile objects
-    //     Tile[] tiles = new Tile[str.length()];
-    //     for (int i = 0; i < str.length(); i++) {
-    //         tiles[i] = Tile.Bag.getBag().getTile(str.charAt(i));
-    //     }
-    //     return tiles;
-    // }
     
     public int getScore(Word word) {
         // Calculate the score for the given word, considering bonus squares
         int score = 0;
         int doublingWord = 1;
-        boolean newWordFlag = true;
         int row = word.getRow();
         int col = word.getCol();
         boolean vertical = word.isVertical();
@@ -356,10 +179,9 @@ public class Board {
             if (tiles[i] == null)
             {
                 tiles[i] = GameBoard[r][c];
-                newWordFlag = false;
             }
 
-            if (r == 7 && c == 7) {
+            if (r == 7 && c == 7 && GameBoard[7][7] == null) {
                 doublingWord *= 2; // Center square (double word score) - star
             }
             if (r == 0 && c == 3 || r == 0 && c == 11 ||
@@ -405,60 +227,31 @@ public class Board {
             }
 
         }
-        if (newWordFlag)
+
         score *= doublingWord;
 
         return score;
     }
-
-    // public int tryPlaceWord(Word word) {
-    //     if (!boardLegal(word)) {
-    //         return 0;
-    //     }
-    
-    //     ArrayList<Word> words = getWords(word);
-    //     int score = 0;
-    
-    //     for (Word w : words) {
-    //         if (!dictionaryLegal(w)) {
-    //             return 0;
-    //         }
-    //         score += getScore(w);
-    //     }
-    
-    //     int row = word.getRow();
-    //     int col = word.getCol();
-    //     Tile[] tiles = word.getTiles();
-    //     boolean vertical = word.isVertical();
-    
-    //     for (int i = 0; i < tiles.length; i++) {
-    //         int r = vertical ? row + i : row;
-    //         int c = vertical ? col : col + i;
-    //         GameBoard[r][c] = tiles[i];
-    //     }
-    
-    //     return score;
-    // }
         
  public int tryPlaceWord(Word word) {
         // Try to place the word on the board and calculate the total score
         if (!boardLegal(word) || !dictionaryLegal(word)) {
             return 0; // Word placement is illegal
         }
-        ArrayList<Word> words;
-        words = getWords(word);
+         
+        ArrayList<Word> words = getWords(word);
         int score = 0;
         for (Word word2 : words) {
             score += getScore(word2);
         }
-        //score += getScore(word);
 
         int row = word.getRow();
         int col = word.getCol();
         // Place the tile on the board
         for (int i=0; i < word.getTiles().length; i++){
-            if (word.tiles[i] != null)
-                GameBoard[row][col] = word.tiles[i];
+            if (word.tiles[i] != null){
+                GameBoard[row][col] = word.tiles[i]; 
+            }
             
             if (word.isVertical())
                 row++;
